eb0a3a5eea2f01442f206d4ee694e87df7e41d68
Tymek Slomski <tymekslomski@gmail.com>
2023-06-05 17:24:35 +0200

    Adding chess pieces to board

--- base.js
+++ base.js
@@ -1,22 +1,22 @@
 const board = document.querySelector(".board");
 
 let startBoard = [
-  "rook",
-  "knight",
-  "bishop",
-  "queen",
-  "king",
-  "bishop",
-  "knight",
-  "rook",
-  "pawn",
-  "pawn",
-  "pawn",
-  "pawn",
-  "pawn",
-  "pawn",
-  "pawn",
-  "pawn",
+  rook,
+  knight,
+  bishop,
+  queen,
+  king,
+  bishop,
+  knight,
+  rook,
+  pawn,
+  pawn,
+  pawn,
+  pawn,
+  pawn,
+  pawn,
+  pawn,
+  pawn,
   "",
   "",
   "",
@@ -49,39 +49,49 @@ let startBoard = [
   "",
   "",
   "",
-  "pawn",
-  "pawn",
-  "pawn",
-  "pawn",
-  "pawn",
-  "pawn",
-  "pawn",
-  "pawn",
-  "rook",
-  "knight",
-  "bishop",
-  "queen",
-  "king",
-  "bishop",
-  "knight",
-  "rook",
+  pawn,
+  pawn,
+  pawn,
+  pawn,
+  pawn,
+  pawn,
+  pawn,
+  pawn,
+  rook,
+  knight,
+  bishop,
+  queen,
+  king,
+  bishop,
+  knight,
+  rook,
 ];
 
 const spawn = () => {
-  startBoard.forEach((element, id) => {
+  startBoard.forEach((element, i) => {
     const cards = document.createElement("div");
     cards.classList.add("card");
-    cards.textContent = element;
-    cards.setAttribute("squerID", id);
+    cards.innerHTML = element;
+    cards.setAttribute("squerId", i);
     board.appendChild(cards);
-    const row = Math.floor((63 - id) / 8) + 1;
+    const row = Math.floor((63 - i) / 8) + 1;
+
+    //set color of board
 
     if (row % 2 == 0) {
-      cards.classList.add(id % 2 == 0 ? "czarne" : "biale");
+      cards.classList.add(i % 2 == 0 ? "darkCards" : "whiteCards");
     } else {
-      cards.classList.add(id % 2 === 0 ? "biale" : "czarne");
+      cards.classList.add(i % 2 === 0 ? "whiteCards" : "darkCards");
+    }
+
+    //set color of piece
+
+    if (i <= 15) {
+      cards.firstChild.firstChild.classList.add("blackPiece");
+    }
+    if (i >= 48) {
+      cards.firstChild.firstChild.classList.add("whitePiece");
     }
   });
 };
 spawn();
-console.log(startBoard[7]);
