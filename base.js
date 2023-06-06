e85b860af606c2b79b772fe161f882cc69eb211e
Tymek Slomski <tymekslomski@gmail.com>
2023-06-06 21:39:48 +0200

    More logick

--- base.js
+++ base.js
@@ -1,4 +1,5 @@
 const board = document.querySelector(".board");
+const pleyer = document.querySelector("#pleyer");
 
 let startBoard = [
   rook,
@@ -66,7 +67,7 @@ let startBoard = [
   knight,
   rook,
 ];
-
+let curentMove = "white";
 const spawn = () => {
   startBoard.forEach((element, i) => {
     const cards = document.createElement("div");
@@ -75,6 +76,8 @@ const spawn = () => {
     cards.setAttribute("squerId", i);
     board.appendChild(cards);
     const row = Math.floor((63 - i) / 8) + 1;
+
+    cards.firstChild?.setAttribute("draggable", true);
 
     //set color of board
 
@@ -95,3 +98,95 @@ const spawn = () => {
   });
 };
 spawn();
+
+let dragElement;
+let startPosition;
+
+const boardSqueres = document.querySelectorAll(".card");
+
+const dragStart = (e) => {
+  startPosition = e.target.parentNode.getAttribute("squerId");
+  dragElement = e.target;
+};
+const dragOver = (e) => {
+  e.preventDefault();
+};
+const dragDrop = (e) => {
+  e.stopPropagation();
+  console.log(dragElement);
+  const corectMove = dragElement.firstChild.classList.contains(curentMove);
+  const taken = e.target.classList.contains("piece");
+  const oponentMove = curentMove === "white" ? "black" : "white";
+  const takenByOponent = e.target.firstChild?.classList.contains(oponentMove);
+  const valid = checkValid(e.target);
+  console.log("oponent", oponentMove);
+  if (corectMove) {
+    console.log("hello");
+    if (takenByOponent && valid) {
+      e.target.parentNode.append(dragElement);
+      e.target.remove();
+      changeColorMove();
+      return;
+    }
+    if (taken && !takenByOponent) {
+      console.log("nie mozesz tul");
+      return;
+    }
+    if (valid) {
+      e.target.append(dragElement);
+      changeColorMove();
+      console.log("work");
+      return;
+    }
+  }
+};
+
+const checkValid = (target) => {
+  const targetId =
+    Number(target.getAttribute("squerId")) ||
+    Number(target.parentNode.getAttribute("squerId"));
+  const startId = Number(startPosition);
+  const piece = dragElement.id;
+  console.log(targetId);
+  console.log(startId);
+  console.log(piece);
+
+  switch (piece) {
+    case "pawn":
+      const startRow = [8, 9, 10, 11, 12, 13, 14, 15];
+      if (startRow.includes(startId) && startId * 8 * 2 === targetId) {
+        return true;
+      }
+  }
+};
+
+boardSqueres.forEach((squer) => {
+  squer.addEventListener("dragstart", dragStart);
+  squer.addEventListener("dragover", dragOver);
+  squer.addEventListener("drop", dragDrop);
+});
+
+const changeColorMove = () => {
+  if (curentMove === "white") {
+    curentMove = "black";
+    changeDirection();
+    pleyer.textContent = curentMove;
+  } else {
+    curentMove = "white";
+    backDirection();
+    pleyer.textContent = curentMove;
+  }
+};
+
+const changeDirection = () => {
+  const allCards = document.querySelectorAll(".card");
+  allCards.forEach((element, i) => {
+    element.setAttribute("squerId", 63 - i);
+  });
+};
+const backDirection = () => {
+  const allCards = document.querySelectorAll(".card");
+  allCards.forEach((element, i) => {
+    element.setAttribute("squerId", i);
+  });
+};
