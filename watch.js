39d9fb7dbcb961db9b6a3d549e3afa7f1bfa112f
Tymek Slomski <tymekslomski@gmail.com>
2023-06-09 22:44:01 +0200

    initial version of the chess stopwatch

--- watch.js
+++ watch.js
@@ -1,37 +1,57 @@
 const pleyerWhite = document.querySelector("#pleyerWhite");
 const time = document.querySelector("#time");
 const pleyerWhiteTime = document.querySelector("#pleyerWhiteTime");
+const pleyerBlackTime = document.querySelector("#pleyerBlackTime");
+
+let interval;
+let isRunning = false;
+let firstRun = true;
 
 const stopWatch = () => {
-  getTime();
+  if (!isRunning) {
+    isRunning = true;
+    getTime();
+  }
 };
 
 const getTime = () => {
+  console.log(isRunning);
   let minutes;
-  let second;
+  let seconds;
+
   let timeString = time.textContent.split("");
   timeString = timeString.filter((dots) => dots !== ":");
 
   if (timeString.length === 3) {
-    minuts = timeString[0];
-    second = timeString.slice(-2).join("");
+    minutes = timeString[0];
+    seconds = timeString.slice(-2).join("");
 
-    let allMinuts = minuts - 1;
-    let intervlMinuts = Number(minuts) * 60;
-    let allTime = 5;
+    let allMinutes;
+    if (firstRun) {
+      allMinutes = minutes - 1;
+      firstRun = false;
+    } else {
+      allMinutes = minutes;
+    }
+    if (seconds == "00") {
+      seconds = 60;
+    }
+    let allTime = seconds;
 
-    let interval = setInterval(() => {
+    interval = setInterval(() => {
       allTime--;
 
-      time.textContent = `${allMinuts}:${allTime.toString().padStart(2, "0")}`;
+      time.textContent = `${allMinutes}:${allTime.toString().padStart(2, "0")}`;
       console.log(allTime.toString().padStart(2, "0"), "alltime");
-      if (allMinuts === 0 && allTime === 0) {
+
+      if (allMinutes === 0 && allTime === 0) {
         console.log("koniec czasu!!!!!!!");
         clearInterval(interval);
+        isRunning = false;
       } else if (allTime === 0) {
-        allTime = 15;
-        allMinuts--;
-        console.log(allMinuts, "allminuts");
+        allTime = 60;
+        allMinutes--;
+        console.log(allMinutes, "allminutes");
       }
     }, 1000);
   }
@@ -39,8 +59,14 @@ const getTime = () => {
   if (timeString.length === 4) {
     minutes = timeString.slice(0, 2).join("");
     seconds = timeString.slice(2).join("");
-    console.log(second);
+    console.log(seconds);
   }
 };
 
+const stopTimer = () => {
+  clearInterval(interval);
+  isRunning = false; // Zatrzymanie odliczania
+};
+
 pleyerWhiteTime.addEventListener("click", stopWatch);
+pleyerBlackTime.addEventListener("click", stopTimer);
