1d4a845dd8038e058c02c2879d9e8517e216f5bd
Tymek Slomski <tymekslomski@gmail.com>
2023-06-10 20:43:52 +0200

    The clock works for two different players

--- watch.js
+++ watch.js
@@ -2,71 +2,139 @@ const pleyerWhite = document.querySelector("#pleyerWhite");
 const time = document.querySelector("#time");
 const pleyerWhiteTime = document.querySelector("#pleyerWhiteTime");
 const pleyerBlackTime = document.querySelector("#pleyerBlackTime");
+const whiteTime = document.querySelector("#whiteTime");
+const blackTime = document.querySelector("#blackTime");
 
-let interval;
-let isRunning = false;
-let firstRun = true;
+let whiteInterval;
+let blackInterval;
+let whiteIsRunning = false;
+let blackIsRunning = false;
+let whiteFirstRun = true;
+let blackFirstRun = true;
+let whitePlay = true;
+let blackPlay = false;
+let whiteFirstClick = true;
+let blackFirstClick = true;
 
-const stopWatch = () => {
-  if (!isRunning) {
-    isRunning = true;
-    getTime();
+const startWhiteWatch = () => {
+  if (!whiteIsRunning) {
+    whiteIsRunning = true;
+    whiteGetTime();
   }
 };
 
-const getTime = () => {
-  console.log(isRunning);
+const startBlackWatch = () => {
+  if (!blackIsRunning) {
+    blackIsRunning = true;
+    blackGetTime();
+  }
+};
+
+const whiteGetTime = () => {
   let minutes;
   let seconds;
 
-  let timeString = time.textContent.split("");
+  let timeString = whiteTime.textContent.split("");
   timeString = timeString.filter((dots) => dots !== ":");
 
-  if (timeString.length === 3) {
+  if (timeString.length === 3 || timeString.length === 4) {
     minutes = timeString[0];
     seconds = timeString.slice(-2).join("");
 
     let allMinutes;
-    if (firstRun) {
+    if (whiteFirstRun) {
       allMinutes = minutes - 1;
-      firstRun = false;
+      whiteFirstRun = false;
     } else {
       allMinutes = minutes;
     }
-    if (seconds == "00") {
+    if (seconds === "00") {
       seconds = 60;
     }
     let allTime = seconds;
 
-    interval = setInterval(() => {
+    whiteInterval = setInterval(() => {
       allTime--;
 
-      time.textContent = `${allMinutes}:${allTime.toString().padStart(2, "0")}`;
-      console.log(allTime.toString().padStart(2, "0"), "alltime");
+      whiteTime.textContent = `${allMinutes}:${allTime
+        .toString()
+        .padStart(2, "0")}`;
 
       if (allMinutes === 0 && allTime === 0) {
-        console.log("koniec czasu!!!!!!!");
-        clearInterval(interval);
-        isRunning = false;
+        console.log("Koniec czasu dla gracza białego!");
+        clearInterval(whiteInterval);
+        whiteIsRunning = false;
       } else if (allTime === 0) {
         allTime = 60;
         allMinutes--;
-        console.log(allMinutes, "allminutes");
       }
     }, 1000);
   }
+};
+const blackGetTime = () => {
+  let minutes;
+  let seconds;
 
-  if (timeString.length === 4) {
-    minutes = timeString.slice(0, 2).join("");
-    seconds = timeString.slice(2).join("");
-    console.log(seconds);
+  let timeString = blackTime.textContent.split("");
+  timeString = timeString.filter((dots) => dots !== ":");
+
+  if (timeString.length === 3 || timeString.length === 4) {
+    minutes = timeString[0];
+    seconds = timeString.slice(-2).join("");
+    let allMinutes;
+    if (blackFirstRun) {
+      allMinutes = minutes - 1;
+      blackFirstRun = false;
+    } else {
+      allMinutes = minutes;
+    }
+    if (seconds === "00") {
+      seconds = 60;
+    }
+    let allTime = seconds;
+
+    blackInterval = setInterval(() => {
+      allTime--;
+
+      blackTime.textContent = `${allMinutes}:${allTime
+        .toString()
+        .padStart(2, "0")}`;
+
+      if (allMinutes === 0 && allTime === 0) {
+        console.log("Koniec czasu dla gracza czarnego!");
+        clearInterval(blackInterval);
+        blackIsRunning = false;
+      } else if (allTime === 0) {
+        allTime = 60;
+        allMinutes--;
+      }
+    }, 1000);
   }
 };
 
-const stopTimer = () => {
-  clearInterval(interval);
-  isRunning = false; // Zatrzymanie odliczania
+const stopWhiteTimer = () => {
+  clearInterval(whiteInterval);
+  whiteIsRunning = false;
 };
 
-pleyerWhiteTime.addEventListener("click", stopWatch);
-pleyerBlackTime.addEventListener("click", stopTimer);
+const stopBlackTimer = () => {
+  clearInterval(blackInterval);
+  blackIsRunning = false;
+};
+
+const switchPlayer = (isWhitePlayer) => {
+  if (isWhitePlayer && !whiteIsRunning) {
+    startWhiteWatch();
+    stopBlackTimer();
+    whitePlay = true;
+    blackPlay = false;
+  } else if (!isWhitePlayer && !blackIsRunning) {
+    startBlackWatch();
+    stopWhiteTimer();
+    whitePlay = false;
+    blackPlay = true;
+  }
+};
+
+pleyerWhiteTime.addEventListener("click", () => switchPlayer(true));
+pleyerBlackTime.addEventListener("click", () => switchPlayer(false));
